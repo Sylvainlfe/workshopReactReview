0bcb3c012adc18a5b7e4c5a2baaa18eba02b3d9f
Enzo Michel <enzomichel399@gmail.com>
2024-05-13 14:00:06 +0200

    fef

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,6 +1,7 @@
 import "../styles/Filter.css";
 
 function Filter({ name }) {
+
   return <button>{name}</button>;
 }
 export default Filter;
--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,10 +1,19 @@
+import { useState } from "react";
 import Filter from "../components/Filter.jsx";
 
 function Filters() {
+  const [nameSeries, setNameSeries] = useState(" ");
+
+  fetch("https://www.amiiboapi.com/api/amiiboseries/")
+    .then((response) => response.json())
+    .then((data) => {
+      setNameSeries(data.amiibo.name);
+    });
+  console.log(nameSeries);
   return (
     <>
-      <Filter name="coucou" />
-      <Filter name="coucou2" />
+      <Filter />
+      <Filter />
     </>
   );
 }
